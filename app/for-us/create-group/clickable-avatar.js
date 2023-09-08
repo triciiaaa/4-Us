577ddac066f4825ae3f3cd03c9cc14ea634ed730
triciiaaa <angjeanee@gmail.com>
2023-09-08 17:58:49 +0800

    added tiktok navbar for home page and username under avatar for create group page

--- app/for-us/create-group/clickable-avatar.js
+++ app/for-us/create-group/clickable-avatar.js
@@ -4,12 +4,12 @@ import Image from "next/image";
 import styles from "../../page.module.css";
 import { Avatar } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { IconButton } from "@mui/material";
+import { IconButton, Stack, Typography } from "@mui/material";
 import RedeemIcon from "@mui/icons-material/Redeem";
 import { useState } from "react";
 import { useTheme } from "@mui/material/styles";
 
-export default function ClickableAvatar({ user }) {
+export default function ClickableAvatar({ id, name }) {
   const theme = useTheme();
   const [isClicked, setIsClicked] = useState(false);
 
@@ -20,19 +20,33 @@ export default function ClickableAvatar({ user }) {
   return (
     <IconButton onClick={handleClick}>
       {isClicked ? (
-        <Avatar
-          alt="User Avatar"
-          sx={{ width: 56, height: 56, bgcolor: theme.palette.blue.dark }}
-        >
-          {user}
-        </Avatar>
+        <Stack gap="5px">
+          <Avatar
+            alt="User Avatar"
+            sx={{ width: 56, height: 56, bgcolor: theme.palette.blue.dark }}
+          >
+            {id}
+          </Avatar>
+          <Typography variant="tag_bold" textAlign="center">
+            {name}
+          </Typography>
+        </Stack>
       ) : (
-        <Avatar
-          alt="User Avatar"
-          sx={{ width: 56, height: 56, bgcolor: theme.palette.blue.unselected }}
-        >
-          {user}
-        </Avatar>
+        <Stack gap="5px">
+          <Avatar
+            alt="User Avatar"
+            sx={{
+              width: 56,
+              height: 56,
+              bgcolor: theme.palette.blue.unselected,
+            }}
+          >
+            {id}
+          </Avatar>
+          <Typography variant="tag_bold" textAlign="center">
+            {name}
+          </Typography>
+        </Stack>
       )}
     </IconButton>
   );
