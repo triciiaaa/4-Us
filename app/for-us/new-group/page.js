2059abec426d8dd53a61657d33f6c98f60f9ff1d
hhuilingg <laihuiling88@gmail.com>
2023-09-08 18:23:15 +0800

    complete new-group, use-profile and product

--- app/for-us/new-group/page.js
+++ app/for-us/new-group/page.js
@@ -1,22 +1,246 @@
 "use client"
 
+import * as React from 'react'; 
 import Image from 'next/image'
 import styles from '../../page.module.css'
 import { Avatar } from '@mui/material'
 import { useRouter } from 'next/navigation'
-import { IconButton } from "@mui/material";
-import RedeemIcon from '@mui/icons-material/Redeem';
-
+import { IconButton, Stack, Typography, CardMedia } from "@mui/material"; 
+import RedeemIcon from '@mui/icons-material/Redeem'
+import FavoriteIcon from "@mui/icons-material/Favorite"
+import TextsmsIcon from "@mui/icons-material/Textsms"
+import BookmarkIcon from "@mui/icons-material/Bookmark"
+import ReplyIcon from "@mui/icons-material/Reply"
+import { useTheme } from "@mui/material/styles";
+ 
 export default function NewGroup() {
   const router = useRouter()
+  const theme = useTheme()
 
-  return (
-    <main className={styles.main}>
-      <div>New Group Page</div>
-      <Avatar onClick={() => router.push('/user-profile')}>H</Avatar>
-      <IconButton aria-label="present" onClick={() => router.push('/for-us/new-group/exchanges')}>
-        <RedeemIcon />
-      </IconButton>
-    </main>
-  )
-}
+  const textStyle = {
+    color: "white",
+  };
+ 
+  return ( 
+      <main className={styles.main}> 
+  
+      <div className={styles.landingImage}>
+        <CardMedia
+          component="img"
+          image={"/cafe_darker.jpg"}
+          title="Cafe"
+          alt="Cafe"
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: 10,
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        > 
+          <Stack gap="15px">
+            <Stack direction="row" alignItems="center" gap="145px"> 
+            <Typography variant="h3" fontSize={30} align='center' style={textStyle} paddingLeft="10px"> 
+            Group Name
+            </Typography>
+          
+            <IconButton aria-label="present" onClick={() => router.push('/for-us/new-group/exchanges')} 
+            style={{
+              color: theme.palette.white.main,
+            }}>
+            <RedeemIcon fontSize="large"/>
+            </IconButton>
+            </Stack>
+        
+            <Stack direction="row" gap="20px">
+              <Image
+                src="/person1.png"
+                width={80}
+                height={80}
+                style={{ borderRadius: "10px" }}
+                alt="Preview"
+                onClick={() => router.push('/user-profile')}
+                />
+              <Image
+                src="/person2.png"
+                width={80}
+                height={80}
+                style={{ borderRadius: "10px" }}
+                alt="Preview"
+                />
+              <Image
+                src="/person3.png"
+                width={80}
+                height={80}
+                style={{ borderRadius: "10px" }}
+                alt="Preview"
+                />
+            </Stack>
+          </Stack>
+
+          <Stack
+            marginLeft="350px"
+            marginTop="230px"
+            gap="10px"
+            direction="flex"
+            flexDirection="column"
+            alignItems="center"
+          >            
+            {/* profile */}
+            <Image
+                src="/person4.png"
+                width={40}
+                height={40}
+                style={{ borderRadius: "10px" }}
+                alt="Preview"
+                onClick={() => router.push('/user-profile')}
+              />
+
+            {/* like */}
+            <Stack
+              direction="flex"
+              flexDirection="column"
+              alignItems="center"
+              gap="0px"
+            >
+              <IconButton
+                aria-label="like"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+                onClick={() => router.push("/tiktok-shop")}
+              >
+                <FavoriteIcon />
+              </IconButton>
+              <Typography
+                variant="tiny_tag_bold"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+              >
+                1392
+              </Typography>
+            </Stack>
+
+            {/* chat */}
+            <Stack
+              direction="flex"
+              flexDirection="column"
+              alignItems="center"
+              gap="0px"
+            >
+              <IconButton
+                aria-label="chat"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+              >
+                <TextsmsIcon />
+              </IconButton>
+              <Typography
+                variant="tiny_tag_bold"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+              >
+                73
+              </Typography>
+            </Stack>
+            
+            {/* save */}
+            <Stack
+              direction="flex"
+              flexDirection="column"
+              alignItems="center"
+              gap="0px"
+            >
+              <IconButton
+                aria-label="save"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+              >
+                <BookmarkIcon />
+              </IconButton>
+              <Typography
+                variant="tiny_tag_bold"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+              >
+                182
+              </Typography>
+
+              {/* send */}
+              <IconButton
+                aria-label="share"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+              >
+                <ReplyIcon />
+              </IconButton>
+              <Typography
+                variant="tiny_tag_bold"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+              >
+                18
+              </Typography>
+            </Stack>
+            </Stack>
+            
+            {/* caption */}
+            <Stack gap="15px">
+            <Stack direction="row" alignItems="center">
+              <Image
+                  src="/person5.png"
+                  width={40}
+                  height={40}
+                  style={{ borderRadius: "10px" }}
+                  alt="Preview"
+                />
+              <Typography
+                variant="body"
+                textAlign="left"
+                paddingLeft="20px"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+              >
+                You saved {'>'}
+              </Typography>
+            </Stack>
+            <Stack gap="3px">
+              <Typography
+                variant="body_bold"
+                textAlign="left"
+                paddingLeft="8px"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+              >
+                @user123456789
+              </Typography>
+              <Typography
+                variant="body_thin"
+                textAlign="left"
+                paddingLeft="8px"
+                style={{
+                  color: theme.palette.white.main,
+                }}
+              >
+                Wow... time to visit this new cafe!
+              </Typography>
+            </Stack>
+          </Stack>
+        </div>
+      </div>
+
+
+      </main>
+    );
+  }
+  
--- app/user-profile/page.js
+++ app/user-profile/page.js
@@ -1,20 +1,154 @@
 "use client"
 
+import React, { useState } from 'react'; 
 import Image from 'next/image'
 import styles from '../page.module.css'
+import { Avatar } from '@mui/material'
 import { useRouter } from 'next/navigation'
-import { IconButton } from "@mui/material";
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { IconButton, Stack, Typography, CardMedia, Box, Checkbox, FormControlLabel, Grid, Paper, } from "@mui/material"; 
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 
 export default function UserProfile() {
   const router = useRouter()
+  const [open, setOpen] = useState(false);
+  const textStyle = {
+    color: "white",
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  
+  const [selectedImages, setSelectedImages] = useState([true, true, true, true, true, false]);
+
+  const images = [
+    '/product1.jpg',
+    '/product2.jpg',
+    '/product3.jpg',
+    '/product4.jpg',
+    '/product5.jpg',
+    '/product6.jpg',
+  ];
 
   return (
     <main className={styles.main}>
-      <div>User Profile Page</div>
-      <IconButton aria-label="add" onClick={() => router.push('/user-profile/wishlist')}>
-        <ShoppingBasketIcon />
-      </IconButton>
+    <Stack
+      direction="column"
+      alignItems="center"
+    >
+      <Box 
+        display="flex"
+        justifyContent="top"  // Horizontally center the image
+        alignItems="top"      // Vertically center the image
+        height="240px"           // Set the desired height
+        paddingTop="40px"
+        paddingRight="80px"
+      >
+      <Image
+        src="/person1.png"
+        width={150}
+        height={150}
+        style={{ maxWidth: '100%', maxHeight: '100%' }}
+        alt="Preview"
+      />
+      <Stack>
+      <Typography
+        variant="body_bold"
+        textAlign="left"
+        paddingTop="35px"
+        paddingLeft="20px"
+        fontSize="30px"
+      >
+        NAME
+      </Typography>
+      <Typography
+        variant="body"
+        textAlign="left"
+        paddingLeft="20px"
+        fontSize="25px"
+      >
+        @user100
+      </Typography>
+    </Stack>
+    </Box>
+
+    <Stack direction="row" alignItems="center">
+    <Image
+      src="/wishlist_icon.jpg"
+      width={50}
+      height={50}
+      style={{ borderRadius: "10px" }}
+      alt="Preview"
+      onClick={() => router.push('/user-profile/wishlist')}
+    />
+    <Typography
+        variant="body"
+        textAlign="left"
+        paddingLeft="10px"
+        fontSize="25px"
+      >
+        My Wishlist
+    </Typography>
+    </Stack>
+    </Stack>
+    <Stack direction="column" paddingTop="20px" gap="20px" alignItems="center">
+    <Grid container spacing={2}>
+      {images.map((image, index) => (
+        <Grid item xs={4} key={index}>
+          <Paper elevation={5}>
+            <Image
+              src={image}
+              width={50}
+              height={60}
+              alt={`Image ${index + 1}`}
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              onClick={() => router.push('/user-profile')}/>
+
+            <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Product</DialogTitle>
+            <DialogContent>
+              {/* Add your content here */}
+              <Stack direction="column" gap="20px">
+              <Image
+                src={image}
+                width={300}
+                height={350}
+                alt={`Image ${index + 1}`}
+              />
+              <Button variant="outlined" color="primary" onClick={() => router.push('/user-profile/product')}>
+              Fulfill the wish!
+              </Button>
+              </Stack>
+
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                  Close
+                </Button>
+              </DialogActions>
+            </Dialog>
+            <Checkbox
+              checked={selectedImages[index]}
+              onChange={() => handleCheckboxChange(index)}
+            />
+          </Paper>
+        </Grid>
+      ))}
+    </Grid>
+    <Typography
+        variant="body"
+        textAlign="left"
+        paddingLeft="20px"
+        fontSize="15px"
+      >
+        View More ...
+    </Typography>
+    </Stack>
     </main>
   )
 }
